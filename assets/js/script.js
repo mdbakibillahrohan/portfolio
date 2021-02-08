5cf0e58f718b690a852d5f2137382953e5ce2911
mdbakibillahrohan <60601661+mdbakibillahrohan@users.noreply.github.com>
2021-02-08 16:47:25 +0600

    Version 1

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoWidth:false,
+        loop:true,
+        dots:true,
+        dotsEach:3,
+        autoplay:true,
+        item:3,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
+    }
+    });
+})
